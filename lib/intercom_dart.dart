f8d5b9b8a5e8f9b04de0dfe0bfbda9dda4f3850e
Max Rozdobudko <max.rozdobudko@gmail.com>
2024-04-12 10:26:23 +0300

    Initial commit.

---
+++ lib/intercom_dart.dart
@@ -0,0 +1,5 @@
+library intercom_dart;
+
+class Client {
+
+}
---
+++ test/intercom_dart_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:intercom_dart/intercom_dart.dart';
+
+void main() {
+
+}
