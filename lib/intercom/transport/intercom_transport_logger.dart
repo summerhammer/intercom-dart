d057596770ccef8fefb2e395612bbdda0f605700
Max Rozdobudko <max.rozdobudko@gmail.com>
2024-04-27 18:23:32 +0300

    refactor: changes `IntercomTransportLogger` method signatures

--- lib/intercom/transport/intercom_transport_logger.dart
+++ lib/intercom/transport/intercom_transport_logger.dart
@@ -3,13 +3,13 @@ abstract interface class IntercomTransportLogger {
   void onRequest(String method, String url, {
     Map<String, dynamic>? query,
     dynamic body,
-    Map<String, dynamic> headers,
+    Map<String, dynamic?> headers,
   });
 
   void onResponse(String method, String url, {
     dynamic body,
     int statusCode,
-    Map<String, dynamic> headers,
+    Map<String, dynamic>? headers,
   });
 
 }
